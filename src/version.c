094cbeb6b5cdbb51647ca4f0d3cdd93605de62e9
Jeff Fulmer <jdfulmer@puppet.armstrong.com>
2015-02-26 15:27:47 -0500

    Routine update: 3.0.9

--- src/version.c
+++ src/version.c
@@ -8,8 +8,8 @@ const char *version_string = "3.0.9";
 const char *program_name   = "siege";
 const char *author_name    = "Jeffrey Fulmer, et al.";
 const char *email_address  = "jeff@joedog.org";
-const char *years          = "1999-2014";
-const char *copyright      = "Copyright (C) 2014 by Jeffrey Fulmer, et al.\n\
+const char *years          = "1999-2015";
+const char *copyright      = "Copyright (C) 2015 by Jeffrey Fulmer, et al.\n\
 This is free software; see the source for copying conditions.\n\
 There is NO warranty; not even for MERCHANTABILITY or FITNESS\n\
 FOR A PARTICULAR PURPOSE.\n";
