09de3395c0b13787dbcb4ef224217b5db9ae4ed9
Jeff Fulmer <jdfulmer@puppet.armstrong.com>
2015-07-08 08:53:30 -0400

    Added additional copyright

--- src/base64.h
+++ src/base64.h
@@ -2,7 +2,9 @@
  * Copyright (c) 1995, 1996, 1997 Kungliga Tekniska Högskolan
  * (Royal Institute of Technology, Stockholm, Sweden).
  * All rights reserved.
- * 
+ *
+ * Portions by Jeffrey Fulmer (c) 2015 
+ *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
  * are met:
