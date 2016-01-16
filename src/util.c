2563b33f610bd228b82ff42fbb78da5f902544c7
Jeff Fulmer <jeff@joedog.org>
2016-01-16 13:42:27 -0500

    Added portable stristr

--- src/util.c
+++ src/util.c
@@ -135,6 +135,22 @@ lowercase(char *s, size_t len){
   return s;
 }
 
+char * 
+stristr(const char* haystack, const char* needle) {
+  do {
+    const char* h = haystack;
+    const char* n = needle;
+    while (tolower((unsigned char) *h) == tolower((unsigned char ) *n) && *n) {
+      h++;
+      n++;
+    }
+    if (*n == 0) {
+      return (char *) haystack;
+    }
+  } while (*haystack++);
+  return 0;
+}
+
 /**
  * sleep and usleep work on all supported
  * platforms except solaris, so we'll use
