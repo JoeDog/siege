bfb556f3cf45f6a585750073b252b519ba2db951
Jeff Fulmer <jeff@joedog.org>
2016-01-28 16:25:05 -0500

    Updated copyright and version: 4.0.0-beta5

--- src/version.c
+++ src/version.c
@@ -4,12 +4,12 @@
  * used by configure to dynamically assign those values 
  * to documentation files.
  */
-const char *version_string = "4.0.0-beta4";
+const char *version_string = "4.0.0-beta5";
 const char *program_name   = "siege";
 const char *author_name    = "Jeffrey Fulmer, et al.";
 const char *email_address  = "jeff@joedog.org";
-const char *years          = "1999-2015";
-const char *copyright      = "Copyright (C) 2015 by Jeffrey Fulmer, et al.\n\
+const char *years          = "1999-2016";
+const char *copyright      = "Copyright (C) 2016 by Jeffrey Fulmer, et al.\n\
 This is free software; see the source for copying conditions.\n\
 There is NO warranty; not even for MERCHANTABILITY or FITNESS\n\
 FOR A PARTICULAR PURPOSE.\n";
