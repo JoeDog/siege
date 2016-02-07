9cda083a520eaabe6e5658aca145493b941e7a72
Jeff Fulmer <jeff@joedog.org>
2016-02-07 12:52:30 -0500

    Added config.h

--- src/url.c
+++ src/url.c
@@ -23,6 +23,10 @@
  * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
  *--
  */
+#ifdef  HAVE_CONFIG_H
+# include <config.h>
+#endif/*HAVE_CONFIG_H*/
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <setup.h>
