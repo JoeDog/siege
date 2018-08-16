cc7493f9b23e35b9b3909bf329cf9428d911abdb
Jeff Fulmer <jdfulmer@armstrong.com>
2018-08-16 08:58:47 -0400

    Version increment: 4.0.4r3

--- src/version.c
+++ src/version.c
@@ -4,7 +4,7 @@
  * used by configure to dynamically assign those values 
  * to documentation files.
  */
-const char *version_string = "4.0.5rc3";
+const char *version_string = "4.0.4rc3";
 const char *program_name   = "siege";
 const char *author_name    = "Jeffrey Fulmer, et al.";
 const char *email_address  = "jeff@joedog.org";
