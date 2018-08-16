a29dee61f8d6c1374d1fcffc410f67802cd79de6
Jeff Fulmer <jdfulmer@armstrong.com>
2018-08-16 08:54:49 -0400

    Version increment 2.0.5r3; copyright update

--- src/version.c
+++ src/version.c
@@ -4,12 +4,12 @@
  * used by configure to dynamically assign those values 
  * to documentation files.
  */
-const char *version_string = "4.0.5rc2";
+const char *version_string = "4.0.5rc3";
 const char *program_name   = "siege";
 const char *author_name    = "Jeffrey Fulmer, et al.";
 const char *email_address  = "jeff@joedog.org";
-const char *years          = "1999-2017";
-const char *copyright      = "Copyright (C) 2017 by Jeffrey Fulmer, et al.\n\
+const char *years          = "1999-2018";
+const char *copyright      = "Copyright (C) 2018 by Jeffrey Fulmer, et al.\n\
 This is free software; see the source for copying conditions.\n\
 There is NO warranty; not even for MERCHANTABILITY or FITNESS\n\
 FOR A PARTICULAR PURPOSE.\n";
