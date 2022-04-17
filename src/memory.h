8735f1872467a4cdcabc3a0a1fc82f8e7263f159
Jeff Fulmer <jeff@joedog.org>
2022-04-17 10:44:47 -0400

    Added xstrncat

--- src/memory.h
+++ src/memory.h
@@ -26,7 +26,8 @@
 
 char * xstrdup(const char *str); 
 char * xstrcat(const char *arg1, ...);
-char * xstrncpy(char* dest, const char*src, size_t n);
+char * xstrncat(char *dest, const char *src, size_t len);
+char * xstrncpy(char* dest, const char *src, size_t len);
 void * xrealloc(void *, size_t);
 void * xmalloc (size_t);
 void * xcalloc (size_t, size_t);  
