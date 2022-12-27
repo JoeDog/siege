4834ee5612ba0bfbad47c372c7e9e1631cb325b6
Jeff Fulmer <jeff@joedog.org>
2022-12-27 12:17:29 -0500

    Routine increment: 4.1.6-b2

--- src/version.c
+++ src/version.c
@@ -4,7 +4,7 @@
  * used by configure to dynamically assign those values 
  * to documentation files.
  */
-const char *version_string = "4.1.6-b1";
+const char *version_string = "4.1.6-b2";
 const char *program_name   = "siege";
 const char *author_name    = "Jeffrey Fulmer, et al.";
 const char *email_address  = "jeff@joedog.org";
