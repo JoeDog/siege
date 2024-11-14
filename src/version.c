941259a9571975d61487ec991e86acd1b975e7e5
Jeff Fulmer <jeff@joedog.org>
2024-11-14 08:58:02 -0500

    Version increment: 4.1.7

--- src/version.c
+++ src/version.c
@@ -4,12 +4,12 @@
  * used by configure to dynamically assign those values 
  * to documentation files.
  */
-const char *version_string = "4.1.7-b6";
+const char *version_string = "4.1.7";
 const char *program_name   = "siege";
 const char *author_name    = "Jeffrey Fulmer, et al.";
 const char *email_address  = "jeff@joedog.org";
-const char *years          = "1999-2023";
-const char *copyright      = "Copyright (C) 2023 by Jeffrey Fulmer, et al.\n\
+const char *years          = "1999-2024";
+const char *copyright      = "Copyright (C) 2024 by Jeffrey Fulmer, et al.\n\
 This is free software; see the source for copying conditions.\n\
 There is NO warranty; not even for MERCHANTABILITY or FITNESS\n\
 FOR A PARTICULAR PURPOSE.\n";
